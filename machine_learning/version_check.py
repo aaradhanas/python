439614877d23c06a8aecf910e15788076296f46a
Aaradhana Sridharan <aaradhana.sridharan@gmail.com>
2018-02-06 23:20:45 +0530

    First ML project

---
+++ machine_learning/version_check.py
@@ -0,0 +1,28 @@
+# Reference : https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+# Installed using the command : python -m pip install numpy scipy matplotlib pandas sklearn
+# Check the versions of libraries
+
+import sys
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+
+#Python
+print('Python : {}'.format(sys.version))
+
+#scipy
+print('scipy : {}'.format(scipy.__version__))
+
+#numpy
+print('numpy : {}'.format(numpy.__version__))
+
+#matplotlib
+print('matplotlib : {}'.format(matplotlib.__version__))
+
+#pandas
+print('pandas : {}'.format(pandas.__version__))
+
+#sklearn
+print('sklearn : {}'.format(sklearn.__version__))
